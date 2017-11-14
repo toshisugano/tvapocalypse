2bb59026b82688af4c4bfedb055ce5ab389600ee
onagus <onagususa@gmail.com>
2017-11-14 03:50:53 -0500

    revs

--- server.js
+++ server.js
@@ -3,6 +3,12 @@ var express = require('express');
 
 var port = process.env.PORT || 8080;    
 
+app.use('/', express.static(__dirname + '/dist'));
+app.use('/css', express.static(__dirname + '/dist/css')); 
+app.use('/scripts', express.static(__dirname + '/dist/scripts')); 
+app.use('/morgue', morgue); 
+
+
 app.get('/', function(req, res ){ 
   res.sendFile(__dirname + '/dist/index.html');
 });   
