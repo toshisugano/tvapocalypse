2159be6e0074cec75d1d641811690978b0721847
onagus <onagususa@gmail.com>
2017-11-14 03:33:32 -0500

    revs

--- server.js
+++ server.js
@@ -1,9 +1,7 @@
-var express = require('express');
-    path = require('path');
+var express = require('express'); 
     app = express(); 
 
-var port = process.env.PORT || 8080;   
-
+var port = process.env.PORT || 8080;    
 
 app.get('/*', function(req, res ){ 
   res.sendFile(__dirname + '/dist/index.html');
