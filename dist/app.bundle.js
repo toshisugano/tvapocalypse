c16753ab3ab6575f6492583b1aec1c48ca3b72ce
onagus <onagususa@gmail.com>
2017-11-14 04:58:58 -0500

    revs

--- dist/app.bundle.js
+++ dist/app.bundle.js
@@ -18806,6 +18806,7 @@ var Item = function (_Component) {
 			var hash = window.location.href;
 			var split = hash.split("");
 			var join = split.slice(22, 33).join("");
+			console.log(join);
 			var url = void 0;
 			var item = void 0;
 			var name = void 0;
